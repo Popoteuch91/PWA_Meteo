76a5476732c716e34b9e662b9a093d3ede041214
Popoteuch91 <aurelien.goulley@gmail.com>
2020-12-02 14:19:37 +0100

    component fiche meteo

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -8,6 +8,7 @@ import { pushFavorites } from "../actions/ville";
 import { KalvinToCelsius, UnixTimeToHour } from "../mixins/functions";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
+import FicheMeteo from "../components/ficheMeteo";
 
 const Search = (props) => {
   const { t, i18n } = useTranslation();
@@ -41,78 +42,8 @@ const Search = (props) => {
   }, [search]);
   return (
     <StyledMain>
-      {meteoVilles.map((meteoVille) => {
-        return (
-          <StyledDiv>
-            <StyledDiv1>
-              <StyledH1>{meteoVille.properties.label}</StyledH1>
-            </StyledDiv1>
-            <StyledDiv2>
-              <StyledDiv2_>
-                <img
-                  src={`http://openweathermap.org/img/wn/${meteoVille.meteo.weather[0].icon}@2x.png`}
-                />
-              </StyledDiv2_>
-              <StyledDiv2_1>
-                <StyledDiv2_2>
-                  {KalvinToCelsius(meteoVille.meteo.main.temp)}°
-                </StyledDiv2_2>
-                <StyledDiv2_3>{meteoVille.meteo.weather[0].main}</StyledDiv2_3>
-              </StyledDiv2_1>
-            </StyledDiv2>
-            <StyledDiv3>
-              <div>
-                <StyledDiv3_>
-                  {KalvinToCelsius(meteoVille.meteo.main.temp_max)}°
-                </StyledDiv3_>
-                <StyledDiv3_1>{t("meteo.temp_max")}</StyledDiv3_1>
-                <StyledDiv3_>
-                  {KalvinToCelsius(meteoVille.meteo.main.temp_min)}°
-                </StyledDiv3_>
-                <StyledDiv3_1>{t("meteo.temp_min")}</StyledDiv3_1>
-              </div>
-              <div>
-                <StyledDiv3_>{meteoVille.meteo.wind.speed}mph</StyledDiv3_>
-                <StyledDiv3_1>{t("meteo.wind")}</StyledDiv3_1>
-                <StyledDiv3_>{meteoVille.meteo.main.humidity}%</StyledDiv3_>
-                <StyledDiv3_1>{t("meteo.humidity")}</StyledDiv3_1>
-              </div>
-              <div>
-                <StyledDiv3_>
-                  {UnixTimeToHour(meteoVille.meteo.sys.sunrise)}
-                </StyledDiv3_>
-                <StyledDiv3_1>{t("meteo.sunrise")}</StyledDiv3_1>
-                <StyledDiv3_>
-                  {UnixTimeToHour(meteoVille.meteo.sys.sunset)}
-                </StyledDiv3_>
-                <StyledDiv3_1>{t("meteo.sunset")}</StyledDiv3_1>
-              </div>
-            </StyledDiv3>
-            <div>
-              <StyledButton
-                onClick={() => {
-                  dispatch(pushFavorites(meteoVille));
-                  history.push("/favorite");
-                }}
-              >
-                {t("favorite.add")}
-              </StyledButton>
-              <StyledButton1
-                onClick={() => {
-                  dispatch(
-                    displayCoordonnees({
-                      lon: meteoVille.geometry.coordinates[0],
-                      lat: meteoVille.geometry.coordinates[1],
-                    })
-                  );
-                  history.push("/meteo");
-                }}
-              >
-                {t("favorite.set_location")}
-              </StyledButton1>
-            </div>
-          </StyledDiv>
-        );
+      {meteoVilles.map((ville) => {
+        return <FicheMeteo ville={ville}></FicheMeteo>;
       })}
     </StyledMain>
   );
@@ -171,139 +102,5 @@ const StyledMain = styled.div`
     padding-left: 5%;
     padding-right: 5%;
   }
-`;
-const StyledDiv = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-
-  @media screen and (min-width: 920px) {
-    width: 880px;
-    margin: 8px auto;
-    font-size: 1.1em;
-    margin-bottom: 0.3em;
-    padding: 0.8em 0.8em;
-    border-radius: 5px;
-    background: #e67d23;
-  }
-  @media (max-width: 1366px) {
-    margin-top: 0.5em;
-    margin-bottom: 0.3em;
-    padding: 0.8em 0.8em;
-    border-radius: 5px;
-    background: #e67d23;
-  }
-  @media (max-width: 1024px) {
-    margin-top: 0.5em;
-    margin-bottom: 0.3em;
-    padding: 0.8em 0.8em;
-    border-radius: 5px;
-    background: #e67d23;
-  }
-  @media (max-width: 768px) {
-    margin-top: 0.5em;
-    margin-bottom: 0.3em;
-    padding: 0.8em 0.8em;
-    border-radius: 5px;
-    background: #e67d23;
-  }
-  @media (max-width: 375px) {
-    margin-top: 1em;
-    margin-bottom: 0.3em;
-    padding: 0.8em 0.8em;
-    border-radius: 5px;
-    background: #e67d23;
-  }
-`;
-const StyledDiv1 = styled.div`
-  width: 100%;
-`;
-const StyledH1 = styled.h1`
-  margin: 0;
-  font-size: 2em;
-  font-weight: 600;
-`;
-const StyledDiv2 = styled.div`
-  display: flex;
-  margin-top: 0.25em;
-  width: 100%;
-  @media screen and (min-width: 700px) {
-    width: 50%;
-  }
-`;
-const StyledDiv2_ = styled.div`
-  flex-grow: 1.25;
-  text-align: center;
-  img {
-    width: 10.5em;
-  }
-`;
-const StyledDiv2_1 = styled.div`
-  flex-grow: 1;
-  text-align: center;
-`;
-const StyledDiv2_2 = styled.div`
-  font-size: 5.25em;
-  font-weight: 300;
-  @media (max-width: 375px) {
-    font-size: 3.25em;
-  }
-`;
-const StyledDiv2_3 = styled.div`
-  margin-top: 0em;
-  margin-left: 0em;
-  text-align: center;
-  font-size: 2em;
-`;
-const StyledDiv3 = styled.div`
-  display: flex;
-  justify-content: space-around;
-  padding-bottom: 1em;
-  width: 100%;
-  border-top: 1px solid rgba(255, 255, 255, 0.5);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
-  @media screen and (min-width: 700px) {
-    margin-bottom: 1em;
-    padding-bottom: 0;
-    border-top: none;
-    border-bottom: none;
-    border-left: 1px solid rgba(255, 255, 255, 0.5);
-  }
-  @media screen and (min-width: 700px) {
-    width: 50%;
-  }
-`;
-const StyledDiv3_ = styled.div`
-  margin-top: 1em;
-  font-size: 1.44em;
-`;
-const StyledDiv3_1 = styled.div`
-  /*color: rgba(255, 255, 255, 0.8);*/
-  @media screen and (min-width: 700px) {
-    display: block;
-  }
-`;
-const StyledButton = styled.button`
-  background: #26a69a;
-  color: white;
-  font-size: 15px;
-  padding: 10px 30px;
-  border: none;
-  border-radius: 3px;
-  font-weight: 900;
-  margin: 20px;
-  cursor: pointer;
-  text-transform: uppercase;
-`;
-const StyledButton1 = styled.button`
-  background: #7e8c8d;
-  color: white;
-  font-size: 15px;
-  padding: 10px 30px;
-  border: none;
-  border-radius: 3px;
-  font-weight: 900;
-  margin: 20px;
-  cursor: pointer;
-  text-transform: uppercase;
 `;
 export default Search;
